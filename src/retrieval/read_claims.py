fd576361b194c5df84480c1c692f55c7cd7c60a7
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2018-07-26 16:16:33 -0700

    more log

--- src/retrieval/read_claims.py
+++ src/retrieval/read_claims.py
@@ -184,19 +184,28 @@ def annotate_save_quit(test_data,logger):
 
     sys.exit(1)
 
+def read_json_alllines(json_file):
+    l=[]
+    with open(json_file) as f:
+            for eachline in (f):
+                d = json.loads(eachline)
+                l.append(d)
+
+    return l
+
 #load predictions, convert it based on label and write it as string.
 def write_pred_str_disk(args,jlr):
     logging.debug("here1"+str(args.out_file))
     final_predictions=[]
     #pred=joblib.load(predicted_results)
     with open(args.out_file,"r") as f:
-        ir = jlr.process(f)
+        ir = json.loads(jlr.process(f))
         logging.debug("here2"+str(len(ir)))
 
         for index,q in enumerate(ir):
             logging.debug("here3")
+
             line=dict()
-            logger.debug("p")
             label="not enough info"
             if(index%2 ==0):
                 logging.debug("here4")
@@ -207,6 +216,9 @@ def write_pred_str_disk(args,jlr):
             line["id"]=q["id"]
             line["predicted_label"]=label
             line["predicted_evidence"]=q["predicted_sentences"]
+            logging.debug(q["predicted_sentences"])
+            logging.debug(q["id"])
+            logging.debug(label)
 
             final_predictions.append(line)
 
@@ -226,7 +238,7 @@ def write_pred_str_disk(args,jlr):
         #
         #     final_predictions.append(line)
 
-    logging.debug(len(final_predictions))
+    logging.info(len(final_predictions))
     with open(args.pred_file, "w+") as out_file:
         for x in final_predictions:
 
