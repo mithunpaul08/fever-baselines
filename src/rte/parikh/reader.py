26b6651f6054cdbaf743ed2439f149963f3e3979
mithunpaul <mithunpaul08@gmail.com>
2018-10-31 23:28:58 -0700

    had misedd out one print statemetn

--- src/rte/parikh/reader.py
+++ src/rte/parikh/reader.py
@@ -108,7 +108,7 @@ class FEVERReader(DatasetReader):
             bodyid = s['Body ID']
             actualBody = d.articles[bodyid]
             label = s['Stance']
-            print(label)
+            
 
             if not (label == "unrelated"):
 
