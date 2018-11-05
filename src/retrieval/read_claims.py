b957260d6a925465b132fd9687bc4e4a68eda489
mithun <mithunpaul08@gmail.com>
2018-11-04 21:53:53 -0700

    fixed:TypeError: write() argument must be str, not int

--- src/retrieval/read_claims.py
+++ src/retrieval/read_claims.py
@@ -330,9 +330,9 @@ class UOFADataReader():
 
     def write_label_to_disk(self, index,label,label_file):
         with open(label_file, "a") as out:
-          out.write(index)
+          out.write(str(index))
           out.write(",")
-          out.write(label)
+          out.write(str(label))
           out.write("\n")
 
     def get_gold_labels(args,jlr):
