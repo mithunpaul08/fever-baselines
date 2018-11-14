9fff0da0d2c91b965339cda876e6104857ce0c6f
mithun <mithunpaul08@gmail.com>
2018-11-14 14:35:10 -0700

    commented out the dev code runner in the file runner_oracle_decomp_args.sh

--- runner_oracle_decomp_args.sh
+++ runner_oracle_decomp_args.sh
@@ -31,10 +31,10 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 #rm -rf logs/
 PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db ${CONFIG} logs/${LOGDIR} --cuda-device $CUDA_DEVICE --mode ${RUNMODE}
-mkdir -p data/models
-cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE}.tar.gz
-echo "Copied model file to data/models/${MODELFILE}.tar.gz"
-PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db data/models/${MODELFILE}.tar.gz
+#mkdir -p data/models
+#cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE}.tar.gz
+#echo "Copied model file to data/models/${MODELFILE}.tar.gz"
+#PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db data/models/${MODELFILE}.tar.gz
 
 
 
