f67cfbb2976ffcfef72b11c69beb77d6cc5d8c09
mithun <mithunpaul08@gmail.com>
2018-11-19 20:52:19 -0700

    will run only dev. will need only the name of the model which in this case should end with oct 23rd

--- runner_oracle_decomp_args.sh
+++ runner_oracle_decomp_args.sh
@@ -28,11 +28,11 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-rm -rf logs/${LOGDIR}
-PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db ${CONFIG} logs/${LOGDIR} --cuda-device $CUDA_DEVICE --mode ${RUNMODE}
-mkdir -p data/models
-cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE}.tar.gz
-echo "Copied model file to data/models/${MODELFILE}.tar.gz"
+##rm -rf logs/${LOGDIR}
+#PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db ${CONFIG} logs/${LOGDIR} --cuda-device $CUDA_DEVICE --mode ${RUNMODE}
+#mkdir -p data/models
+#cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE}.tar.gz
+#echo "Copied model file to data/models/${MODELFILE}.tar.gz"
 PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db data/models/${MODELFILE}.tar.gz data/fever/dev.ns.pages.p1.jsonl
 
 
