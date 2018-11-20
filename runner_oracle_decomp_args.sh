f7c17b4998c302044c3617acad80d8b3a7405595
mithun <mithunpaul08@gmail.com>
2018-11-20 16:43:46 -0700

    will run dev. Will exit after printing overlaps. Created a folder on jenny called dev_branch_temp. This is a copy of the dev folder. HOwever, rememeber to delete it soon. I have to do this like this because righ tnow the dev branch is running all the random seed+ training slice experiment in tmux 6. I don’t want to touch it. But still want to work on the printing part. I don’t want to create a new branch to test this alone...maybe i should?

--- runner_oracle_decomp_args.sh
+++ runner_oracle_decomp_args.sh
@@ -38,18 +38,17 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-for RANDOMSEED in 98467 12459 58354
-do
-    for SLICE in 10 25 50 75
-    do
-        rm -rf logs/${LOGDIR}
-        PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db ${CONFIG} logs/${LOGDIR} --cuda-device $CUDA_DEVICE --mode ${RUNMODE} --randomseed ${RANDOMSEED} --slice ${SLICE}
-        mkdir -p data/models
-        MODELFILE_NAME=${MODELFILE}_${RANDOMSEED}_${SLICE}
-        cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE_NAME}.tar.gz
-        echo "Copied model file to data/models/${MODELFILE_NAME}.tar.gz"
-        PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db data/models/${MODELFILE_NAME}.tar.gz data/fever/dev.ns.pages.p1.jsonl --param_path ${CONFIG} --randomseed ${RANDOMSEED} --slice ${SLICE}
+#for RANDOMSEED in 98467 12459 58354
+##   for SLICE in 10 25 50 75
+#  do
+#rm -rf logs/${LOGDIR}
+#PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db ${CONFIG} logs/${LOGDIR} --cuda-device $CUDA_DEVICE --mode ${RUNMODE} --randomseed ${RANDOMSEED} --slice ${SLICE}
+#mkdir -p data/models
+#MODELFILE_NAME=${MODELFILE}_${RANDOMSEED}_${SLICE}
+#cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE_NAME}.tar.gz
+#echo "Copied model file to data/models/${MODELFILE_NAME}.tar.gz"
+PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db data/models/${MODELFILE_NAME}.tar.gz data/fever/dev.ns.pages.p1.jsonl --param_path ${CONFIG} --randomseed ${RANDOMSEED} --slice ${SLICE}
 
-    done
-done
+#    done
+#done
 
