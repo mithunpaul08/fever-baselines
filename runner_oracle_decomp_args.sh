bb4c1e6fd0b98f4ef48e1cc83213f8a5245a2294
mithun <mithunpaul08@gmail.com>
2018-11-19 23:47:02 -0700

    will run the whole train-dev cycle. This is mainly to check if the folder is created with random seed name

--- runner_oracle_decomp_args.sh
+++ runner_oracle_decomp_args.sh
@@ -35,11 +35,11 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 rm -rf logs/${LOGDIR}
 PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db ${CONFIG} logs/${LOGDIR} --cuda-device $CUDA_DEVICE --mode ${RUNMODE} --randomseed ${RANDOMSEED}
-#mkdir -p data/models
-#MODELFILE_RANDOMSEED=${MODELFILE}_${RANDOMSEED}
-#cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE_RANDOMSEED}.tar.gz
-#echo "Copied model file to data/models/${MODELFILE_RANDOMSEED}.tar.gz"
-#PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db data/models/${MODELFILE}.tar.gz data/fever/dev.ns.pages.p1.jsonl --param_path ${CONFIG}
+mkdir -p data/models
+MODELFILE_RANDOMSEED=${MODELFILE}_${RANDOMSEED}
+cp logs/${LOGDIR}/model.tar.gz data/models/${MODELFILE_RANDOMSEED}.tar.gz
+echo "Copied model file to data/models/${MODELFILE_RANDOMSEED}.tar.gz"
+PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db data/models/${MODELFILE}.tar.gz data/fever/dev.ns.pages.p1.jsonl --param_path ${CONFIG}
 
 
 
