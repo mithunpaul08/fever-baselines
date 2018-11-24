3a822b524860e10efdef1ad5edc526d78c4d2c57
mithunpaul <mithunpaul08@gmail.com>
2018-11-23 20:12:50 -0700

    removed sys.exit. the value of the lmode also is now added to config file

--- src/scripts/rte/da/eval_da.py
+++ src/scripts/rte/da/eval_da.py
@@ -283,17 +283,17 @@ if __name__ == "__main__":
     slice = uofa_params.pop('training_slice_percent', {})
     random_seed = uofa_params.pop('random_seed', {})
     name_of_trained_model_to_use = uofa_params.pop('name_of_trained_model_to_use', {})
-
     path_to_pyproc_annotated_data_folder = uofa_params.pop('path_to_pyproc_annotated_data_folder', {})
+    debug_mode = uofa_params.pop('debug_mode', {})
 
 
     log_file_name = "dev_feverlog.txt" + str(slice) + "_" + str(random_seed)
-    mithun_logger = setup_custom_logger('root', args.lmode,log_file_name)
+    mithun_logger = setup_custom_logger('root', debug_mode,log_file_name)
+
 
     mithun_logger.info("inside main function going to call eval on "+str(dataset_to_test))
     mithun_logger.info("path_to_pyproc_annotated_data_folder " + str(path_to_pyproc_annotated_data_folder))
     mithun_logger.info("value of name_of_trained_model_to_use " + str(name_of_trained_model_to_use))
-    sys.exit(1)
 
 
 
