cb1b1fbc4d8a169a17e25eb6a35f65515276de5b
mithunpaul <mithunpaul08@gmail.com>
2018-12-08 19:59:28 -0700

    WILL PRINT THE First 20 claim annotation pairs

--- run_fact_verif.py
+++ run_fact_verif.py
@@ -70,7 +70,8 @@ def generate_features(zipped_annotated_data,feature,feature_details,reader,mithu
                                                                                                                       be_split,
                                                                                                                       hl_split,
                                                                                                                       bl_split,hw_split, bw_split)
-        # if(index %100==0):
+        if(index==0):
+            sys.exit()
         #     mithun_logger.info(f"value of old label is:{label}")
         #     mithun_logger.info(f"value of new label is:{new_label}")
         #     mithun_logger.info(f"value of claim before annotation is:{hw}")
